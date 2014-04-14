1ccc2d2fd546c098052bb8ef3fe40a38c269a429
conan <conan.cook@gmail.com>
2014-04-14 23:24:45 +0100

    Cleanup unwanted code

--- src/it/core.clj
+++ src/it/core.clj
@@ -10,9 +10,9 @@
 (def server (atom nil))
 
 (defn start-server
+  "Start a ring server and store a reference"
   []
-  (swap! server (fn [_] (run-jetty handler {:port 3000 :join? false})))
-  (println "Server started"))
+  (swap! server (fn [_] (run-jetty handler {:port 3000 :join? false}))))
 
 (defn stop-server
   []
